68ff2da99c50724784aa21d84eaa86cd64f6ccba
faishalyw2910 <2300018330@webmail.uad.ac.id>
2025-01-12 20:47:17 +0700

    faishal

--- MainGUI.java
+++ MainGUI.java
@@ -1,4 +1,12 @@
+
 public class MainGUI {
-    public static void main(String[] args) {
-        
-}
+    private static List<Pengunjung> pengunjungList = new ArrayList<>();//agregasi
+    private static List<Pemandu> pemanduList = new ArrayList<>();  //agregsi
+    private static LaporanPendapatan lpd = new LaporanPendapatan();
+    private static List<zonaWisata> zonaList = new ArrayList<>();//agregasi
+    private static Fasilitas fasilitas = new Fasilitas() {
+        @Override
+        public String getLayanan() {
+            return "";
+        }
+    };
